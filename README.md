c1f7a4903191b9062a2429023bf0e98e7c79c28f
Nurayend <sabyrova04@mail.ru>
2021-02-24 01:07:10 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# appstory
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/appstory)
---
+++ src/app/products.ts
@@ -0,0 +1,120 @@
+export const products = [
+  {
+    id: 1,
+    name: 'The Very Hungry Caterpillar',
+    price: 799,
+    rating: 4.9,
+    description: 'THE all-time classic picture book, from generation to generation, sold somewhere in the world every 30 seconds! A sturdy and beautiful book to give as a gift for new babies, baby showers, birthdays, and other new beginnings!',
+    link: 'https://www.amazon.com/Very-Hungry-Caterpillar-Eric-Carle/dp/0399226907/ref=sr_1_1?dchild=1&keywords=The+Very+Hungry+Caterpillar&qid=1614101740&s=books&sr=1-1',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/41tyokViuNL._SY355_BO1,204,203,200_.jpg'],
+    image2: ['https://images-na.ssl-images-amazon.com/images/I/51IYXiM7ioL.jpg'],
+    image3:
+    ['https://images-na.ssl-images-amazon.com/images/I/81EDMJza+xL.jpg']
+  },
+  {
+    id: 2,
+    name: 'Goodnight Moon',
+    price: 699,
+    rating: 5.0,
+    description: 'In this classic of childrens literature, beloved by generations of readers and listeners, the quiet poetry of the words and the gentle, lulling illustrations combine to make a perfect book for the end of the day.',
+    link: 'https://www.amazon.com/dp/0694003611/ref=s9_acsd_ri_bw_c2_x_3_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-12&pf_rd_r=S82ZVD0BWP9XD8VA8X85&pf_rd_t=101&pf_rd_p=5ce6d8b8-5077-495c-9f75-aa09194de846&pf_rd_i=283155',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/51Ix49rxgtL._SY414_BO1,204,203,200_.jpg'],
+    image2: ['https://images-na.ssl-images-amazon.com/images/I/81HX-KwoTYL.jpg'],
+    image3: ['https://images-na.ssl-images-amazon.com/images/I/41GJ4RRW6VL.jpg']
+  },
+  {
+    id: 3,
+    name: 'Love You Forever',
+    price: 299,
+    rating: 4.3,
+    description: 'So begins the story that has touched the hearts of millions worldwide. Since publication in l986, Love You Forever has sold more than 30 million copies in paperback and the regular hardcover edition (as well as hundreds of thousands of copies in Spanish and French).',
+    link: 'https://www.amazon.com/dp/0920668372/ref=s9_acsd_ri_bw_c2_x_4_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-12&pf_rd_r=S82ZVD0BWP9XD8VA8X85&pf_rd_t=101&pf_rd_p=5ce6d8b8-5077-495c-9f75-aa09194de846&pf_rd_i=283155',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/81vpsIs58WL.jpg'],
+    image2: ['https://images-na.ssl-images-amazon.com/images/I/51btzMipeFL.jpg'],
+    image3: ['https://images-na.ssl-images-amazon.com/images/I/71X3kCt5TnL.jpg']
+  },
+  {
+    id: 4,
+    name: 'Think Again: The Power of Knowing What You Dont Know',
+    price: 299,
+    rating: 4.3,
+    description: 'Intelligence is usually seen as the ability to think and learn, but in a rapidly changing world, there is another set of cognitive skills that might matter more: the ability to rethink and unlearn. In our daily lives, too many of us favor the comfort of conviction over the discomfort of doubt. We listen to opinions that make us feel good, instead of ideas that make us think hard.',
+    link: 'https://www.amazon.com/dp/1984878107/ref=s9_acsd_ri_bw_c2_x_7_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-12&pf_rd_r=S82ZVD0BWP9XD8VA8X85&pf_rd_t=101&pf_rd_p=5ce6d8b8-5077-495c-9f75-aa09194de846&pf_rd_i=283155',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/41bChkOa4zL._SX329_BO1,204,203,200_.jpg'],
+    image2: ['https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77702074037.jpg'],
+    image3: ['https://cdn.hipwallpaper.com/i/14/53/Wxz90T.jpg']
+  },
+  {
+    id: 5,
+    name: 'Where is Spot?',
+    price: 299,
+    rating: 4.3,
+    description: 'With its bold blue cover, this board book edition of Eric Hill’s classic story featuring everyone’s favorite puppy is just as iconic as the first lift-the-flap edition.',
+    link: 'https://www.amazon.com/dp/1984878107/ref=s9_acsd_ri_bw_c2_x_7_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-12&pf_rd_r=S82ZVD0BWP9XD8VA8X85&pf_rd_t=101&pf_rd_p=5ce6d8b8-5077-495c-9f75-aa09194de846&pf_rd_i=283155',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/51amEygdqQL._SX483_BO1,204,203,200_.jpg'],
+    image2: ['https://images-na.ssl-images-amazon.com/images/I/51iUjPYh5FL.jpg'],
+    image3: ['https://images-na.ssl-images-amazon.com/images/I/41BfQ+oM5ZL.jpg']
+  },
+  {
+    id: 6,
+    name: 'The Giving Tree',
+    price: 299,
+    rating: 4.3,
+    description: 'The Giving Tree, a story of unforgettable perception, beautifully written and illustrated by the gifted and versatile Shel Silverstein, has been a classic favorite for generations.',
+    link: 'https://www.amazon.com/dp/1984878107/ref=s9_acsd_ri_bw_c2_x_7_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-12&pf_rd_r=S82ZVD0BWP9XD8VA8X85&pf_rd_t=101&pf_rd_p=5ce6d8b8-5077-495c-9f75-aa09194de846&pf_rd_i=283155',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/41fR2KV7jzL._SX376_BO1,204,203,200_.jpg'],
+    image2: ['https://images-na.ssl-images-amazon.com/images/I/81CLVUGaRZL.jpg'],
+    image3: ['https://images-na.ssl-images-amazon.com/images/I/31vTM89aO-L.jpg']
+  },
+  {
+    id: 7,
+    name: 'The Four Winds: A Novel',
+    price: 299,
+    rating: 4.8,
+    description: 'From the number-one bestselling author of The Nightingale and The Great Alone comes a powerful American epic about love and heroism and hope, set during the Great Depression, a time when the country was in crisis and at war with itself, when millions were out of work and even the land seemed to have turned against them.',
+    link: 'https://www.amazon.com/dp/1250178606/ref=s9_acsd_ri_bw_c2_x_2_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-10&pf_rd_r=S82ZVD0BWP9XD8VA8X85&pf_rd_t=101&pf_rd_p=d2e01c79-7462-4300-8d41-3633536344dc&pf_rd_i=283155',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/51r9ZiBU6rL._SX327_BO1,204,203,200_.jpg'],
+    image2: ['https://images-na.ssl-images-amazon.com/images/I/81C+2Ts2-nL.jpg'],
+    image3: ['https://quotefancy.com/media/wallpaper/1600x900/723344-Julia-Quinn-Quote-The-Smythe-Smith-musicale-Thankfully-it-came.jpg']
+  },
+  {
+    id: 8,
+    name: 'Dr. Seusss Beginner Book Collection (Cat in the Hat, One Fish Two Fish, Green Eggs and Ham, Hop on Pop, Fox in Socks)',
+    price: 299,
+    rating: 4.3,
+    description: 'This collectible Dr. Seuss boxed set is perfect for inspiring a love of reading, and with five books in one super giftable set, it will complete any beginning readers shelf!',
+    link: 'https://www.amazon.com/dp/1984878107/ref=s9_acsd_ri_bw_c2_x_7_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-12&pf_rd_r=S82ZVD0BWP9XD8VA8X85&pf_rd_t=101&pf_rd_p=5ce6d8b8-5077-495c-9f75-aa09194de846&pf_rd_i=283155',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/81FxtWFGiiL.jpg'],
+    image2: ['https://images-na.ssl-images-amazon.com/images/I/81eyvBglfbL.jpg'],
+    image3: ['https://images-na.ssl-images-amazon.com/images/I/81uYtyU3lNL.jpg']
+  },
+  {
+    id: 9,
+    name: 'Brown Bear, Brown Bear, What Do You See?',
+    price: 299,
+    rating: 4.3,
+    description: 'A big happy frog, a plump purple cat, a handsome blue horse, and a soft yellow duck--all parade across the pages of this delightful book. Children will immediately respond to Eric Carles flat, boldly colored collages. Combined with Bill Martins singsong text, they create unforgettable images of these endearing animals.',
+    link: 'https://www.amazon.com/dp/0805047905/ref=s9_acsd_ri_bw_c2_x_1_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-12&pf_rd_r=S82ZVD0BWP9XD8VA8X85&pf_rd_t=101&pf_rd_p=5ce6d8b8-5077-495c-9f75-aa09194de846&pf_rd_i=283155',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/81EVdWdmOKL.jpg'],
+    image2: ['https://images-na.ssl-images-amazon.com/images/I/51f1UfYBF0L.jpg'],
+    image3: ['https://images-na.ssl-images-amazon.com/images/I/41qzXK9Kz-L.jpg']
+  },
+  {
+    id: 10,
+    name: 'See, Touch, Feel: A First Sensory Book',
+    price: 299,
+    rating: 4.3,
+    description: 'There is lots for baby to explore in See, Touch, Feel: A First Sensory Book. This sturdy board book, with bright photographs of happy babies, is specially designed to stimulate curiosity through sensory play. Each page has a colorful picture activity that invites baby to touch and explore. There are raised textures to feel, finger trails to follow, and a shiny mirror to look in to. Can they follow the swirl on the snail is shell. Can they tap the drum?',
+    link: 'https://www.amazon.com/dp/0312527594/ref=s9_acsd_ri_bw_c2_x_2_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-12&pf_rd_r=S82ZVD0BWP9XD8VA8X85&pf_rd_t=101&pf_rd_p=5ce6d8b8-5077-495c-9f75-aa09194de846&pf_rd_i=283155',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/51QM2+Zrf2L._SY451_BO1,204,203,200_.jpg'],
+    image2: ['https://images-na.ssl-images-amazon.com/images/I/71gQwQzRPML.jpg'],
+    image3: ['https://cdn.wallpapersafari.com/77/24/27DVkJ.jpg']
+  },
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
